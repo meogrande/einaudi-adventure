a9ab3205adef41cce6472f8e9f9ad087568d24df
Fabio <fabio.biscaro@gmail.com>
2016-11-24 08:35:14 +0100

    Refactoring codice ConnessioneMysql

--- base/src/it/fabiobiscaro/database/ConnessioneMysql.java
+++ base/src/it/fabiobiscaro/database/ConnessioneMysql.java
@@ -1,6 +1,5 @@
 package it.fabiobiscaro.database;
 
-import java.io.IOException;
 import java.sql.*;
 
 public class ConnessioneMysql {
@@ -17,25 +16,19 @@ public class ConnessioneMysql {
 			System.err.println(e.getMessage());
 		} // fine try-catch
 
-		try {
-			System.out.println(new java.io.File("src").getCanonicalPath());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-
-		
-			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/peer?user=root&password=");
-			//jdbc:mysql://localhost:3306/Contatti?user=root&password=secret
-		
+		// Creo la connessione al database
+		cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/peer?user=root&password=");
+		// peer è il nome del database
 
 		sql = "SELECT * FROM corso;";
 		// ________________________________query
 		try {
-			st = cn.createStatement();
-			rs = st.executeQuery(sql);
-			while (rs.next() == true)
+			st = cn.createStatement(); // creo sempre uno statement sulla
+										// connessione
+			rs = st.executeQuery(sql); // faccio la query su uno statement
+			while (rs.next() == true) {
 				System.out.println(rs.getString("id") + "\t" + rs.getString("idTutor"));
+			}
 		} catch (SQLException e) {
 			System.out.println("errore:" + e.getMessage());
 		} // fine try-catch
