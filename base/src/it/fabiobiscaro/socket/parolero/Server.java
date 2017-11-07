4ff9ab91f7897f2913a69162a36ef8bda2b23f94
Fabio <fabio.biscaro@gmail.com>
2017-11-07 10:51:30 +0100

    Piccola descrizione del parolero aggiunta

--- base/src/it/fabiobiscaro/socket/parolero/Server.java
+++ base/src/it/fabiobiscaro/socket/parolero/Server.java
@@ -95,7 +95,7 @@ public class Server {
 	public static void main(String[] args) {
 		// Avvio il server
 		System.out.println("Avvia il server e due client. Connettili e "
-				+ "poi scrivi la parola nella calsella del client. "
+				+ "poi scrivi la parola che il server fornisce nella calsella del client. "
 				+ "Il server controllerà se la parola scritta è uguale.");
 		Server server = new Server();
 		server.avvio();
